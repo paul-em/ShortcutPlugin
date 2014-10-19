f7308c2dfa17ffac2baed7253904ead50d3dd2ca
Paul Em <paul3m@gmail.com>
2014-10-19 21:20:52 +0200

    added possibility for custom icon

--- src/android/ShortcutPlugin.java
+++ src/android/ShortcutPlugin.java
@@ -9,67 +9,94 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Intent;
 import android.content.Context;
-import android.os.Parcelable; 
+import android.os.Parcelable;
 import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager;
 
 public class ShortcutPlugin extends CordovaPlugin {
-    public static final String ACTION_ADD_SHORTCUT = "addShortcut"; 
-    public static final String ACTION_DEL_SHORTCUT = "delShortcut"; 
+	public static final String ACTION_ADD_SHORTCUT = "addShortcut";
+	public static final String ACTION_DEL_SHORTCUT = "delShortcut";
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
 
-        try {
-            if (ACTION_ADD_SHORTCUT.equals(action)) {
+		try {
+			if (ACTION_ADD_SHORTCUT.equals(action)) {
 
-		//Get params
-                JSONObject arg_object = args.getJSONObject(0);
+				// Get params
+				JSONObject arg_object = args.getJSONObject(0);
 
-                Context context=this.cordova.getActivity().getApplicationContext();
-		PackageManager pm = context.getPackageManager();
+				// set param defaults
+				String shortcuttext = arg_object.getString("shortcuttext");
+				String icon = arg_object.getString("icon");
 
-		Intent i = new Intent();
-		i.setClassName(this.cordova.getActivity().getPackageName(), this.cordova.getActivity().getClass().getName());
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-		Intent shortcutintent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-		shortcutintent.putExtra("duplicate", false);
-		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_NAME, arg_object.getString("shortcuttext"));
+				Context context = this.cordova.getActivity()
+						.getApplicationContext();
+				PackageManager pm = context.getPackageManager();
 
-		//Get Icon
-		ResolveInfo ri = pm.resolveActivity(i, 0);
-		int iconId = ri.activityInfo.applicationInfo.icon;
-		Parcelable icon = Intent.ShortcutIconResource.fromContext(context, iconId);
+				Intent i = new Intent();
+				i.setClassName(this.cordova.getActivity().getPackageName(),
+						this.cordova.getActivity().getClass().getName());
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
-		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, i);
-		context.sendBroadcast(shortcutintent);
+				Intent shortcutintent = new Intent(
+						"com.android.launcher.action.INSTALL_SHORTCUT");
+				shortcutintent.putExtra("duplicate", false);
+				shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_NAME,
+						arg_object.getString("shortcuttext"));
 
-                callbackContext.success();
-                return true;
-            } else if (ACTION_DEL_SHORTCUT.equals(action)) {
-                JSONObject arg_object = args.getJSONObject(0);
-		Context context=this.cordova.getActivity().getApplicationContext();
+				// Get Icon
+				if(isNull(icon)){
+					ResolveInfo ri = pm.resolveActivity(i, 0);
+					int iconId = ri.activityInfo.applicationInfo.icon;
+					Parcelable icon = Intent.ShortcutIconResource.fromContext(
+						context, iconId);
+		        	shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+				} else {
+					Drawable iconDrawable = decodeBase64(icon);
+					BitmapDrawable bd = (BitmapDrawable) iconDrawable;
+					shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bd.getBitmap());
+				}
 
-		Intent i = new Intent();
-		i.setClassName(this.cordova.getActivity().getPackageName(), this.cordova.getActivity().getClass().getName());
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, i);
+				context.sendBroadcast(shortcutintent);
 
-		Intent shortcutintent = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT");
-		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_NAME, arg_object.getString("shortcuttext"));
-		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, i);
-                context.sendBroadcast(shortcutintent);
-                callbackContext.success();
-            } 
-            callbackContext.error("Invalid action");
-            return false;
-        } catch(Exception e) {
-            System.err.println("Exception: " + e.getMessage());
-            callbackContext.error(e.getMessage());
-            return false;
-        } 
+				callbackContext.success();
+				return true;
+			} else if (ACTION_DEL_SHORTCUT.equals(action)) {
+				JSONObject arg_object = args.getJSONObject(0);
+				Context context = this.cordova.getActivity()
+						.getApplicationContext();
+
+				Intent i = new Intent();
+				i.setClassName(this.cordova.getActivity().getPackageName(),
+						this.cordova.getActivity().getClass().getName());
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+				Intent shortcutintent = new Intent(
+						"com.android.launcher.action.UNINSTALL_SHORTCUT");
+				shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_NAME,
+						arg_object.getString("shortcuttext"));
+				shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, i);
+				context.sendBroadcast(shortcutintent);
+				callbackContext.success();
+			}
+			callbackContext.error("Invalid action");
+			return false;
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			callbackContext.error(e.getMessage());
+			return false;
+		}
+	}
+
+	private static Bitmap decodeBase64(String input)
+    {
+        byte[] decodedByte = Base64.decode(input, 0);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
     }
 }
--- www/ShortcutPlugin.js
+++ www/ShortcutPlugin.js
@@ -1,33 +1,45 @@
 //Copyright 2013 Jorge Cisneros jorgecis@gmail.com
 
-var ShortcutPlugin = function() {}; 
-
-ShortcutPlugin.prototype.CreateShortcut = function (shortcut_text, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'ShortcutPlugin',
-            'addShortcut',
-            [{ 
-                "shortcuttext": shortcut_text
-            }]
-        );
-};
-ShortcutPlugin.prototype.RemoveShortcut = function(shortcut_text, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'ShortcutPlugin',
-            'delShortcut',
-            [{ 
-                "shortcuttext": shortcut_text
-            }]
-        );
+var ShortcutPlugin = function () {
 };
 
-if(!window.plugins) {
-    window.plugins = {};
+ShortcutPlugin.prototype.CreateShortcut = function (data, successCallback, errorCallback) {
+
+  // to provide backwards compatibility
+  if (typeof data === 'String') {
+    data = {
+      shortcuttext: data
+    };
+  }
+
+  if (typeof data !== 'Object' || typeof data.shortcuttext !== 'String') {
+    errorCallback('required shortcuttext is not set or not a string');
+    return;
+  }
+
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    'ShortcutPlugin',
+    'addShortcut',
+    [data]
+  );
+};
+ShortcutPlugin.prototype.RemoveShortcut = function (shortcut_text, successCallback, errorCallback) {
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    'ShortcutPlugin',
+    'delShortcut',
+    [{
+      "shortcuttext": shortcut_text
+    }]
+  );
+};
+
+if (!window.plugins) {
+  window.plugins = {};
 }
 if (!window.plugins.Shortcut) {
-    window.plugins.Shortcut = new ShortcutPlugin();
+  window.plugins.Shortcut = new ShortcutPlugin();
 }
